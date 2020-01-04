328a62b515bfc1504a6374f113645dbf21ab4c63
aoumeior <aoumeior@outlook.com>
2020-01-04 16:15:38 +0800

    1002

--- 1002/A+B_for_Polynomials.cpp
+++ 1002/A+B_for_Polynomials.cpp
@@ -3,18 +3,18 @@
 #include <vector>
 #include <algorithm>
 #include <math.h> 
-
+#include <iomanip>
 int main()
 {
 	size_t i;
-	std::map<int, double > A1;
-	std::map<int, double > A2;
+	std::map<int, double> A1;
+	std::map<int, double> A2;
 	std::vector<int> temp;
 	std::cin >> i;
 	for (int c = i; c > 0; c--)
 	{
 		int a;
-		float b;
+		double b;
 		std::cin >> a;
 		std::cin >> b;
 		A1.insert({ a, b });
@@ -23,7 +23,7 @@ int main()
 	for (int c = i; c > 0; c--)
 	{
 		int a;
-		float b;
+		double b;
 		std::cin >> a;
 		std::cin >> b;
 		A2.insert({ a, b });
@@ -35,7 +35,7 @@ int main()
 		if (A2.find(it.first) != A2.end())
 		{
 			it.second += A2[it.first];
-			if (fabs(it.second) < 1e-2)
+			if (fabs(it.second) <= 0)
 				A1.erase(it.first);
 		}
 	}
@@ -44,12 +44,13 @@ int main()
 	{
 		if (std::find(temp.begin(), temp.end(), it.first) != temp.end())
 			continue;
-		A1[it.first] = it.second;
+		if (fabs(it.second) > 0)
+			A1.insert({it.first,it.second});
 	}
 
 	std::cout << A1.size();
 	std::for_each(A1.rbegin(), A1.rend(),
 		[](std::pair<const int, double > it) {
-		std::cout << " " << it.first << " " << it.second;
+		std::cout << ' ' << it.first << ' ' << std::fixed << std::setprecision(1) << it.second;
 	});
 }
