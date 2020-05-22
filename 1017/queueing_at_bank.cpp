84cee82530b494e9007497786c843853f2357a4b
aoumeior <aoumeior@outlook.com>
2020-05-22 13:35:39 +0800

    ❤ fix 1017

--- 1017/queueing_at_bank.cpp
+++ 1017/queueing_at_bank.cpp
@@ -6,17 +6,38 @@
 #include <algorithm>
 #include <cmath>
 #include <iomanip>
+#include <queue>
 
-constexpr std::int16_t open_time = 8 * 60 * 60;
-
+constexpr std::int32_t open_time = 8 * 60 * 60;
+constexpr std::int32_t last_time = 17 * 60 * 60;
 struct queueing_at_bank
 {
-    std::int16_t customer_arriving_time;
-    std::int16_t service_time;
-    queueing_at_bank(std::int16_t a, std::int16_t b)
+    std::int32_t customer_arriving_time;
+    std::int32_t service_time; // finish
+    queueing_at_bank(std::int32_t a, std::int32_t b)
         : customer_arriving_time(a),
           service_time(b)
     {
+    }
+};
+
+struct window
+{
+    int total;
+
+    window(int _t) : total(_t) {}
+
+    bool operator<(const window &w) const
+    {
+
+        if (total > w.total)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 };
 
@@ -26,39 +47,50 @@ int main()
     std::cin >> total_number_of_customers >> number_of_windows;
 
     std::vector<queueing_at_bank> today;
-    std::int16_t h, m, s, p, customer_arriving_time;
+    std::int32_t h, m, s, p;
 
     for (size_t i = 0; i < total_number_of_customers; i++)
     {
         std::scanf("%d:%d:%d %d", &h, &m, &s, &p);
-        customer_arriving_time = h * 60 * 60 + m * 60 + s * 60 - open_time;
-        today.push_back({customer_arriving_time, p});
+        today.push_back({(h * 60 * 60 + m * 60 + s), p * 60});
     }
     std::sort(today.begin(), today.end(), [](const queueing_at_bank &left, const queueing_at_bank &right) {
         return left.customer_arriving_time > right.customer_arriving_time;
     });
 
-    std::uint32_t sum = 0;
-    std::uint16_t number = number_of_windows;
-    std::uint16_t last_finish = 0;
+    std::int32_t sum = 0;
 
     queueing_at_bank t(0, 0);
-    for (; today.empty(); today.pop_back())
+    int cnt = 0;
+    std::priority_queue<window> windows;
+    for (int i = 0; i < number_of_windows; i++)
+    {
+        windows.push(window(open_time));
+    }
+
+    while (!today.empty())
     {
         t = today.back();
-        number--;
-        if (number == 0)
-        {
-            sum += (last_finish - t.customer_arriving_time > 0 ? last_finish - t.customer_arriving_time : 0) 
-                    + t.service_time > 60 ? 60 * 60 : t.service_time * 60;
-            last_finish =((last_finish - t.customer_arriving_time > 0 )? last_finish : t.customer_arriving_time) + t.service_time > 60 ? 60 * 60 : t.service_time * 60;
-            number = number_of_windows - 1;
-            continue;
-        }
-        if(t.customer_arriving_time >)
-        last_finish = std::max(((std::int32_t)t.customer_arriving_time > 0 ? (std::int32_t)t.customer_arriving_time : 0) + t.service_time * 60, (std::int32_t)last_finish);
+        today.pop_back();
+        if (t.customer_arriving_time > last_time)
+            break;
+        cnt++;
 
+        auto w = windows.top();
+        windows.pop();
+
+        if (t.customer_arriving_time < w.total)
+        {
+            sum += w.total - t.customer_arriving_time;
+            w.total += t.service_time;
+        }
+        else
+        {
+            w.total = t.customer_arriving_time + t.service_time;
+        }
+        windows.push(w);
     }
-    std::cout << std::fixed << std::setprecision(1) << sum / 60 / customer_arriving_time;
+    double result = sum / 60.f / cnt;
+    std::cout << std::fixed << std::setprecision(1) << result << std::endl;
     return 0;
 }
